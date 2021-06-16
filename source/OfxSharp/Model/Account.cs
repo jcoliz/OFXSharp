22ab3da51a5febd3710d6aa5ca9b69b75ade52fd
Dai Rees <dai@dai.me>
2021-06-15 23:25:25 -0700

    Grouping and moving files to subfolders.

---
+++ source/OfxSharp/Model/Account.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Xml;
+
+namespace OfxSharp
+{
+    public class Account
+    {
+        public Account(XmlNode node, AccountType type)
+        {
+            this.AccountType = type;
+
+            this.AccountId = node.GetValue("//ACCTID");
+            this.AccountKey = node.GetValue("//ACCTKEY");
+
+            switch ( this.AccountType )
+            {
+                case AccountType.BANK:
+                this.InitializeBank(node);
+                    break;
+
+                case AccountType.AP:
+                this.InitializeAp(node);
+                    break;
+
+                case AccountType.AR:
+                this.InitializeAr(node);
+                    break;
+            }
+        }
+
+        public string AccountId { get; set; }
+        public string AccountKey { get; set; }
+        public AccountType AccountType { get; set; }
+
+        /// <summary>
+        ///     Initializes information specific to bank
+        /// </summary>
+        private void InitializeBank(XmlNode node)
+        {
+            this.BankId = node.GetValue("//BANKID");
+            this.BranchId = node.GetValue("//BRANCHID");
+
+            //Get Bank Account Type from XML
+            String bankAccountType = node.GetValue("//ACCTTYPE");
+
+            //Check that it has been set
+            if (string.IsNullOrEmpty(bankAccountType))
+            {
+                throw new OFXParseException("Bank Account type unknown");
+            }
+
+            //Set bank account enum
+            this._bankAccountType = bankAccountType.GetBankAccountType();
+        }
+
+        #region Bank Only
+
+        private BankAccountType _bankAccountType = BankAccountType.NA;
+
+        public string BankId { get; set; }
+
+        public string BranchId { get; set; }
+
+        public BankAccountType BankAccountType
+        {
+            get => this.AccountType == AccountType.BANK ? this._bankAccountType : BankAccountType.NA;
+            set => this._bankAccountType = this.AccountType == AccountType.BANK ? value : BankAccountType.NA;
+        }
+
+        #endregion Bank Only
+
+        #region Account types not supported
+
+        private void InitializeAp(XmlNode node)
+        {
+            throw new OFXParseException("AP Account type not supported");
+        }
+
+        private void InitializeAr(XmlNode node)
+        {
+            throw new OFXParseException("AR Account type not supported");
+        }
+
+        #endregion Account types not supported
+    }
+}
---
+++ source/OfxSharp/Parser/Extensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Xml;
+
+namespace OfxSharp
+{
+	internal static class Extensions
+	{
+		public static string Fmt(this string format, params object[] args)
+		{
+			return String.Format( CultureInfo.CurrentCulture, format, args );
+		}
+
+		public static string FmtInv(this string format, params object[] args)
+		{
+			return String.Format( CultureInfo.InvariantCulture, format, args );
+		}
+
+        public static bool IsSet(this string s)
+		{
+			return !String.IsNullOrWhiteSpace(s);
+		}
+
+        public static bool IsEmpty(this string s)
+		{
+			return String.IsNullOrWhiteSpace(s);
+		}
+
+        public static TEnum ParseEnum<TEnum>(this string s)
+           where TEnum : struct, Enum
+        {
+            if( Enum.TryParse<TEnum>( s, ignoreCase: true, out TEnum value ) )
+            {
+                return value;
+            }
+            else
+            {
+                throw new FormatException( "Couldn't parse \"{0}\" as a {1} enum value.".Fmt( s, typeof(TEnum).Name ) );
+            }
+        }
+
+        public static TEnum? MaybeParseEnum<TEnum>(this string s)
+            where TEnum : struct, Enum
+        {
+            return Enum.TryParse<TEnum>( s, ignoreCase: true, out TEnum value ) ? value : (TEnum?)null;
+        }
+
+        /// <summary> Returns value of specified node</summary>
+        /// <param name="node">Node to look for specified node</param>
+        /// <param name="xpath">XPath for node you want</param>
+        public static string GetValue(this XmlNode node, string xpath)
+        {
+            // TODO: Be more rigorous about XmlNode types. `FirstChild` is meant to be the #text node, not an element node, right?
+            // ...in which case, why not use `XmlNode.InnerText`?
+
+			if( node.SelectSingleNode( xpath ) is XmlNode selectedNode )
+            {
+                if( selectedNode.FirstChild is XmlNode firstChild )
+                {
+                    return firstChild.Value;
+                }
+            }
+                
+            return string.Empty;
+        }
+	}
+}
