3217e80fff2fa923f89193aa69731e91ae8fefc9
James Coliz <jcoliz@outlook.com>
2022-01-24 19:14:53 -0800

    Trim unused constructors from OfxException

--- source/OfxSharp/Parser/OFXException.cs
+++ source/OfxSharp/Parser/OFXException.cs
@@ -6,19 +6,7 @@ namespace OfxSharp
     [Serializable]
     public class OfxException : Exception
     {
-        public OfxException()
-        {
-        }
-
         public OfxException(string message) : base(message)
-        {
-        }
-
-        public OfxException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        protected OfxException(SerializationInfo info,StreamingContext context) : base(info, context)
         {
         }
     }
