b38903fc6e04bc9565627203f997d890f9e8b1e9
James Coliz <jcoliz@outlook.com>
2022-01-25 11:15:24 -0800

    Added InvestementStatements member

--- source/OfxSharp/Model/OFXDocument.cs
+++ source/OfxSharp/Model/OFXDocument.cs
@@ -15,10 +15,16 @@ namespace OfxSharp
             XmlElement signOnMessageResponse = ofxElement.RequireSingleElementChild("SIGNONMSGSRSV1");
             XmlElement bankMessageResponse   = ofxElement.GetSingleElementChildOrNull("BANKMSGSRSV1");
 
-            return new OfxDocument(
+            var result = new OfxDocument(
                 signOn    : SignOnResponse.FromXmlElement( signOnMessageResponse ),
                 statements: GetStatements( bankMessageResponse )
             );
+
+            XmlElement invMessageResponse     = ofxElement.GetSingleElementChildOrNull("INVSTMTMSGSRSV1");
+            if ( ! ( invMessageResponse is null ))
+                result.InvestementStatements = GetInvestmentStatements( invMessageResponse );
+
+            return result;
         }
 
         public static IEnumerable<OfxStatementResponse> GetStatements( XmlElement bankMessageResponse )
@@ -31,11 +37,18 @@ namespace OfxSharp
                 )
                 ?? Enumerable.Empty<OfxStatementResponse>();
 
+        private static IEnumerable<OfxInvestmentStatementResponse> GetInvestmentStatements( XmlElement invMessageResponse )
+        {
+            return Enumerable.Empty<OfxInvestmentStatementResponse>();
+        }
+
         public OfxDocument( SignOnResponse signOn, IEnumerable<OfxStatementResponse> statements )
         {
             this.SignOn = signOn ?? throw new ArgumentNullException( nameof( signOn ) );
 
             this.Statements = new List<OfxStatementResponse>( statements );
+
+            this.InvestementStatements = Enumerable.Empty<OfxInvestmentStatementResponse>();
         }
 
         /// <summary>SIGNONMSGSRSV1. Required. Cannot be null.</summary>
@@ -44,7 +57,8 @@ namespace OfxSharp
         /// <summary>BANKMSGSRSV1/STMTTRNRS</summary>
         public IEnumerable<OfxStatementResponse> Statements { get; private set; }
 
-        //
+        public IEnumerable<OfxInvestmentStatementResponse> InvestementStatements { get; private set; }
+
 
         public Boolean HasSingleStatement( out SingleStatementOfxDocument doc )
         {
---
+++ source/OfxSharp/Model/OfxInvestmentStatementResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace OfxSharp
+{
+    public class OfxInvestmentStatementResponse
+    {
+        internal static OfxStatementResponse FromINVSTMTRS( XmlElement invstmtrs )
+        {
+            throw new NotImplementedException();
+#if false
+            _ = invstmtrs.AssertIsElement( "INVSTMTTRNRS", parentElementName: "INVSTMTMSGSRSV1" );
+
+            XmlElement stmtrs    = invstmtrs.RequireSingleElementChild("STMTRS");
+            XmlElement transList = stmtrs  .RequireSingleElementChild("BANKTRANLIST");
+#endif
+        }
+    }
+}
