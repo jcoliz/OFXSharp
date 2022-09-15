512978cdb0b8af97ddd0358495497a9e19dc9624
technicaljohn <TechnicalJohn@users.noreply.github.com>
2022-09-15 11:06:53 -0500

    Started work on making message types more flexible - Added class with definitions

---
+++ source/OfxSharp/Model/MessageResponseSets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfxSharp
+{
+    /// <summary>
+    ///     Dictionary of message types and transaction types
+    /// </summary>
+    public class MessageResponseSets
+    {
+        public IDictionary<string, ResponseSet> getMessageResponseSets()
+        {
+            var knownSets = new Dictionary<string, ResponseSet>()
+            {
+                {"BANKMSGSRSV1", new ResponseSet {StatementTransactionRecord="STMTTRNRS", StatementRecord="STMTRS"} },
+                {"CREDITCARDMSGSRSV1", new ResponseSet{StatementTransactionRecord="CCSTMTTRNRS", StatementRecord="CCSTMTRS"} }
+            };
+            return knownSets;
+        }
+    }
+
+    public class ResponseSet
+    {
+        public string StatementTransactionRecord { get; set; }
+        public string StatementRecord { get; set; }
+    }
+}
